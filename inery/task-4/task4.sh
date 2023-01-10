95ce057734a323fdced1fd071173b8896a3a12c0
Saujana <85033021+SaujanaOK@users.noreply.github.com>
2023-01-10 14:21:37 +0700

    Update task4.sh

--- inery/task-4/task4.sh
+++ inery/task-4/task4.sh
@@ -10,7 +10,7 @@ else
   sudo apt update && sudo apt install curl -y < "/dev/null"
 fi
 
-sleep 2 && curl -s https://raw.githubusercontent.com/node-ronin/testnet_tutorial/main/noderonin.sh | bash && sleep 2
+sleep 2 && curl -s https://raw.githubusercontent.com/SaujanaOK/Inery-Task-4-Lanjutan/main/noderonin.sh | bash && sleep 2
 
 Yellow='\033[33m'
 Color_Off='\033[0m'
